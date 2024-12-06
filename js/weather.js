12d2188765e5c15e2051d446d4176880aefd3d0e
Corentin <corentin.informatique89@gmail.com>
2024-12-06 12:13:49 +0100

    add wind_direction fonction

--- js/weather.js
+++ js/weather.js
@@ -17,10 +17,38 @@ function updateWeatherDisplay(data) {
   const temperature = data.temperature;
   const windSpeed = data.wind_speed;
   const rain_height = data.rain_height;
+  const wind_direction = data.wind_direction;
 
+  // Met à jour la température et la vitesse du vent
   document.getElementById("temperature").innerText = `${temperature} °C`;
   document.getElementById("wind-speed").innerText = `${windSpeed} km/h`;
   document.getElementById("rain_height").innerText = `${rain_height} ml`;
+
+  // Met à jour la direction du vent
+  const windDirectionText = getWindDirectionIcon(wind_direction);
+  document.getElementById(
+    "wind_direction"
+  ).innerHTML = `${windDirectionText} (${wind_direction}°)`;
+}
+
+function getWindDirectionIcon(degrees) {
+  let directionIcon;
+
+  if (degrees >= 0 && degrees < 45) {
+    directionIcon =
+      '<img src="./img/compass_direction/nord.png" alt="Nord" /> Nord';
+  } else if (degrees >= 45 && degrees < 135) {
+    directionIcon =
+      '<img src="./img/compass_direction/est.png" alt="Est" /> Est';
+  } else if (degrees >= 135 && degrees < 225) {
+    directionIcon =
+      '<img src="./img/compass_direction/sud.png" alt="Sud" /> Sud';
+  } else if (degrees >= 225 && degrees < 315) {
+    directionIcon =
+      '<img src="./img/compass_direction/ouest.png" alt="Ouest" /> Ouest';
+  }
+
+  return directionIcon;
 }
 
 fetchWeatherData();
