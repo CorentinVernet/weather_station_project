d37f970c2ea3e5e885fdd590d9061e3d9f4c79f2
Corentin <corentin.informatique89@gmail.com>
2025-05-19 15:20:24 +0200

    no use folder and add arduino code

--- arduino/wind_direction-wind_speed/wind_direction-wind_speed.ino
+++ arduino/wind_direction-wind_speed/wind_direction-wind_speed.ino
@@ -7,26 +7,26 @@
 
 LiquidCrystal_I2C lcd(0x27, 20, 4); // Adresse I2C de l'écran LCD et dimensions
 
- /*
-DHT dht(13, DHT22); // Capteur DHT22 (Température, Humidité)
-Adafruit_BMP085_Unified bmp = Adafruit_BMP085_Unified(10085); // Baromètre
+/*
+  DHT dht(13, DHT22); // Capteur DHT22 (Température, Humidité)
+  Adafruit_BMP085_Unified bmp = Adafruit_BMP085_Unified(10085); // Baromètre
 
-#define VALEUR_PLUVIOMETRE 1.9 // mm par bascule
-volatile unsigned int countPluviometre = 0;
-float pluvio1min = 0;
- */
+  #define VALEUR_PLUVIOMETRE 1.9 // mm par bascule
+  volatile unsigned int countPluviometre = 0;
+  float pluvio1min = 0;
+*/
 
 unsigned long previousMillis2 = 0;
 const long delaiProgramme = 5000;
 
- /*
-const int ledPin = 12; // LED CO2
- */
+/*
+  const int ledPin = 12; // LED CO2
+*/
 
 void countRain() {
   // Interrupt pluviomètre (non utilisée)
   /*
-  countPluviometre++;
+    countPluviometre++;
   */
 }
 
@@ -40,8 +40,8 @@ const int directionPins[numDirections] = {4, 5, 6, 7, 8, 9, 10, 11};
 const char* directions[numDirections] = {"N", "NE", "E", "SE", "S", "SO", "O", "NO"};
 
 /*
-const int analogPin = A0;
-int sensorValue = 0;
+  const int analogPin = A0;
+  int sensorValue = 0;
 */
 
 void interruption0() {
@@ -55,11 +55,11 @@ void setup() {
   Wire.begin();
 
   /*
-  dht.begin();
-  pinMode(3, INPUT_PULLUP); // Pluviomètre
-  pinMode(analogPin, INPUT); // Capteur gaz
-  pinMode(ledPin, OUTPUT); // LED
-  attachInterrupt(digitalPinToInterrupt(3), countRain, FALLING);
+    dht.begin();
+    pinMode(3, INPUT_PULLUP); // Pluviomètre
+    pinMode(analogPin, INPUT); // Capteur gaz
+    pinMode(ledPin, OUTPUT); // LED
+    attachInterrupt(digitalPinToInterrupt(3), countRain, FALLING);
   */
 
   pinMode(2, INPUT_PULLUP); // Anémomètre
@@ -71,17 +71,17 @@ void setup() {
 }
 
 void loop() {
-  /* 
-  float t = dht.readTemperature();
-  float h = dht.readHumidity();
-  sensorValue = analogRead(analogPin);
-  float voltage = sensorValue * (5.0 / 1023.0);
-  float co2_ppm = voltage * 412;
+  /*
+    float t = dht.readTemperature();
+    float h = dht.readHumidity();
+    sensorValue = analogRead(analogPin);
+    float voltage = sensorValue * (5.0 / 1023.0);
+    float co2_ppm = voltage * 412;
 
-  sensors_event_t event;
-  bmp.getEvent(&event);
-  float temperature;
-  bmp.getTemperature(&temperature);
+    sensors_event_t event;
+    bmp.getEvent(&event);
+    float temperature;
+    bmp.getTemperature(&temperature);
   */
 
   int directionIndex = getDirectionIndex();
@@ -90,9 +90,9 @@ void loop() {
   if (currentMillis - previousMillis2 >= delaiProgramme) {
     previousMillis2 = currentMillis;
 
-    /* 
-    pluvio1min = countPluviometre * VALEUR_PLUVIOMETRE;
-    countPluviometre = 0;
+    /*
+      pluvio1min = countPluviometre * VALEUR_PLUVIOMETRE;
+      countPluviometre = 0;
     */
 
     Serial.print("Vitesse du vent en Km/h : ");
@@ -106,28 +106,28 @@ void loop() {
   }
 
   lcd.clear();
-  /* 
-  lcd.setCursor(0, 0);
-  lcd.print("T= ");
-  lcd.print(t);
-  lcd.print("C");
-  lcd.setCursor(10, 0);
-  lcd.print("CO2= ");
-  lcd.print(co2_ppm);
+  /*
+    lcd.setCursor(0, 0);
+    lcd.print("T= ");
+    lcd.print(t);
+    lcd.print("C");
+    lcd.setCursor(10, 0);
+    lcd.print("CO2= ");
+    lcd.print(co2_ppm);
 
-  lcd.setCursor(0, 1);
-  lcd.print("H= ");
-  lcd.print(h);
-  lcd.print("% ");
-  lcd.setCursor(0, 3);
-  lcd.print("Press= ");
-  lcd.print(event.pressure * 432);
-  lcd.print(" hPa");
+    lcd.setCursor(0, 1);
+    lcd.print("H= ");
+    lcd.print(h);
+    lcd.print("% ");
+    lcd.setCursor(0, 3);
+    lcd.print("Press= ");
+    lcd.print(event.pressure * 432);
+    lcd.print(" hPa");
 
-  lcd.setCursor(10, 1);
-  lcd.print("P= ");
-  lcd.print(pluvio1min);
-  lcd.print("mm");
+    lcd.setCursor(10, 1);
+    lcd.print("P= ");
+    lcd.print(pluvio1min);
+    lcd.print("mm");
   */
 
   // Affichage vitesse et direction du vent
@@ -144,12 +144,12 @@ void loop() {
     lcd.print("??");
   }
 
-  /* 
-  if (co2_ppm > 1000) {
+  /*
+    if (co2_ppm > 1000) {
     digitalWrite(ledPin, HIGH);
-  } else {
+    } else {
     digitalWrite(ledPin, LOW);
-  }
+    }
   */
 
   tour = 0;
@@ -158,7 +158,7 @@ void loop() {
 
 int getDirectionIndex() {
   for (int i = 0; i < numDirections; i++) {
-    if (digitalRead(directionPins[i]) == LOW) {
+    if (digitalRead(directionPins[i]) == HIGH) {
       return i;
     }
   }
